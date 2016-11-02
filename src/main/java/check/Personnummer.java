53c4aaa66fb5183390534b99d0dc4d0536425987
Alex <alex.rasten@outlook.com>
2016-11-02 09:14:46 +0100

    fler operationer

--- src/main/java/check/Personnummer.java
+++ src/main/java/check/Personnummer.java
@@ -21,7 +21,6 @@ import java.util.Date;
 @Path("/kontrollera")
 public class Personnummer {
 
-
     @Path("/koll/{personNummer}")
     @GET
     @Produces("application/json")
@@ -30,19 +29,65 @@ public class Personnummer {
         JSONObject jsonObject = new JSONObject();
         Response resp;
         jsonObject.put("Fick in Value", personNummer);
-        String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n" + jsonObject;
+        String result = "@Produces(\"application/json\") Output: \n\n" + jsonObject;
         if(personNummer.length() != 10 && personNummer.length() != 12) {
            resp  = Response.status(500).entity(result).build();
             return resp;
         }
+
         resp = Response.status(200).entity(result).build();
         return resp;
     }
 
-    @Path("/alder/{personNummer}")
+    @Path("/alder/{personNummer/json}")
     @GET
     @Produces("application/json")
     public Response returneraalder(@PathParam("personNummer") String personNummer) throws JSONException {
+
+        JSONObject jsonObject = new JSONObject();
+        Response resp;
+        Date d = new Date();
+        if(personNummer.length() != 10 && personNummer.length() != 12) {
+            jsonObject.put("Meep in Value", personNummer);
+            String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n" + jsonObject;
+
+            resp  = Response.status(500).entity(result).build();
+            return resp;
+        }
+
+
+        if(personNummer.length() == 10) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd");
+            try {
+                d = sdf.parse(personNummer.substring(0,1) + "-" + personNummer.substring(2,3) +"-" + personNummer.substring(4,5));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(personNummer.length() == 12) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            try {
+                d = sdf.parse(personNummer.substring(0,4) + "-" + personNummer.substring(4,6) + "-" + personNummer.substring(6,8));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+        LocalDate birthDate = new LocalDate(d);
+        LocalDate now = new LocalDate(LocalDate.now());
+        Years age = Years.yearsBetween(birthDate,now);
+
+        jsonObject.put("Ålder", age.getYears());
+        String result2 = "@Produces(\"application/json\") Output: \n\n" + jsonObject;
+
+        resp = Response.status(200).entity(result2).build();
+        return resp;
+    }
+    @Path("/alder/{personNummer/xml}")
+    @GET
+    @Produces("application/xml")
+    public Response returneraalderXml(@PathParam("personNummer") String personNummer) throws JSONException {
 
         JSONObject jsonObject = new JSONObject();
         Response resp;
--- src/test/java/check/PersonnummerTest.java
+++ src/test/java/check/PersonnummerTest.java
@@ -1,5 +1,7 @@
 package check;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,6 +10,17 @@ import static org.junit.Assert.*;
  * Created by eara02 on 2016-10-31.
  */
 public class PersonnummerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+
     @Test
     public void returneraPersonnummer() throws Exception {
 
@@ -16,7 +29,7 @@ public class PersonnummerTest {
     @Test
     public void returneraalder() throws Exception {
         check.Personnummer p = new check.Personnummer();
-        p.returneraalder("201201010212");
+        p.returneraalder("194501010212");
     }
 
 }
